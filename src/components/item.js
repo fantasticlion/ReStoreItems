a9c12af4557ea31780a609d7914c3b967925a5af
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2024-08-16 10:44:39 -0500

    Update item.js

--- src/components/item.js
+++ src/components/item.js
@@ -7,10 +7,10 @@ export default function ProductItem(props) {
         <li>Product Type: {props.info["product_type"]}</li>
         <li>Processor: {props.info["processor"]}</li>
         <li>Release Time: {props.info["release_time"]}</li>
-        <li>Slogan: {props.info["slogan"]}</li>
+        <li>Note: {props.info["note"]}</li>
         <li>Price: ${props.info["price"]}</li>
       </ul>
       <button onClick={() => props.setStateOfParent(props.info["name"], props.info["price"])}>
-        {props.added === 1 ? "Remove from Favorites" : "Add to Favorites"}</button>
+        {props.added === 1 ? "Remove" : "Calculate Total"}</button>
     </div>);
 }
