712edeaa3a231ea5fe5787dbe67c677db1b41dce
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2024-08-16 09:24:05 -0500

    Added showing and hiding nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import productsData from "./assets/items.json";
 import ProductItem from "./components/item";
 
@@ -10,11 +10,12 @@ function App() {
   const [filter, setFilter] = useState({"release_time": [], "product_type": [], "processor": []});
   const [filterData, setFilterData] = useState(productsData);
   const [total, setTotal] = useState(0);
+  const [showNav, setShowNav] = useState(true); // State to manage navigation bar visibility
 
   const allSorts = {
-    productType: {method: (a, b) => (parseFloat(a.id) < parseFloat(b.id) ? -1 : 1)},
-    ascending: {method: (a, b) => (parseFloat(a.price) < parseFloat(b.price) ? -1 : 1)},
-    descending: {method: (a, b) => (parseFloat(a.price) > parseFloat(b.price) ? -1 : 1)},
+    productType: { method: (a, b) => (parseFloat(a.id) < parseFloat(b.id) ? -1 : 1) },
+    ascending: { method: (a, b) => (parseFloat(a.price) < parseFloat(b.price) ? -1 : 1) },
+    descending: { method: (a, b) => (parseFloat(a.price) > parseFloat(b.price) ? -1 : 1) },
   };
 
   const allFilters = 
@@ -74,69 +75,75 @@ function App() {
   }
 
   return (
-    <div className = "App">
-      <div className = "product-cards">
-        <h1>Green Country ReStores &nbsp;
+    <div className="App">
+      <div className="product-cards">
+        <h1>Green Country ReStores  
           <img src="https://images.squarespace-cdn.com/content/v1/60148fcfca55b203f218fe44/49cb0b5c-21d6-4bf8-8f74-544cd98ae908/GC_ReStoreLogoBlackHome.png" 
-            height = "66" padding="100"></img>
+            height="66" padding="100" alt="ReStore Logo"></img>
         </h1>
-        <div className = "product"> {
+        <button onClick={() => setShowNav(!showNav)} style={{ position: 'absolute', top: 10, right: 10 }}>
+          {showNav ? 'Hide Filters' : 'Show Filters'}
+        </button>
+        <div className="product"> {
           filterData.sort(allSorts[sort].method)
-            .map ((item, index) => (<ProductItem key={"product" + index} info = {item} 
-              added = {favorites[item.name]} setStateOfParent={updateFavorites}/>))}
+            .map((item, index) => (<ProductItem key={"product" + index} info={item} 
+              added={favorites[item.name]} setStateOfParent={updateFavorites}/>))}
         </div>
       </div>
 
-      <div className="favorites">
-        <form>
-          <div className="sorting">
-            <h3>Sort By:</h3>
-            <input class = "jss4" type = "radio" value = "productType" defaultChecked name = "sort" onClick = {() => setSort("productType")}></input>
-            <label> Product Type </label><br/>
-            <input class = "jss4" type = "radio" value = "ascending" name = "sort" onClick = {() => setSort("ascending")}></input>
-            <label> Price: Ascending </label><br/>
-            <input class = "jss4" type = "radio" value = "descending" name = "sort" onClick = {() => setSort("descending")}></input>
-            <label> Price: Descending </label><br/>
-          </div>
+      {showNav && (
+        <div className="favorites">
+          <form>
+            <div className="sorting">
+              <h3>Sort By:</h3>
+              <input className="jss4" type="radio" value="productType" defaultChecked name="sort" onClick={() => setSort("productType")}></input>
+              <label> Product Type </label><br/>
+              <input className="jss4" type="radio" value="ascending" name="sort" onClick={() => setSort("ascending")}></input>
+              <label> Price: Ascending </label><br/>
+              <input className="jss4" type="radio" value="descending" name="sort" onClick={() => setSort("descending")}></input>
+              <label> Price: Descending </label><br/>
+            </div>
 
-          <div className="filtering">
-            <h3>Product Type:</h3>
-            <input type = "checkbox" value = "Furniture" onClick = {() => updateFilter("Furniture", "product_type")}/>
-            <label> Furniture </label><br/>
-            <input type = "checkbox" value = "Clothes" onClick = {() => updateFilter("Clothes", "product_type")}/> 
-            <label> Clothes </label><br/>
-            <input type = "checkbox" value = "Accessories" onClick = {() => updateFilter("Accessories", "product_type")}/> 
-            <label> Accessories </label><br/>
-            <input type = "checkbox" value = "Fitness" onClick = {() => updateFilter("Fitness", "product_type")}/> 
-            <label> Fitness </label><br/>
-            <input type = "checkbox" value = "Other" onClick = {() => updateFilter("Other", "product_type")}/> 
-            <label> Other </label><br/>
-            
-            <h3>Release Time:</h3>
-            <input type = "checkbox" value = "8/14" onClick = {() => updateFilter("8/14", "release_time")}/> 
-            <label> 8/14 </label><br/>
-            <input type = "checkbox" value = "8/15" onClick = {() => updateFilter("8/15", "release_time")}/> 
-            <label> 8/15 </label><br/>
+            <div className="filtering">
+              <h3>Product Type:</h3>
+              <input type="checkbox" value="Furniture" onClick={() => updateFilter("Furniture", "product_type")}/>
+              <label> Furniture </label><br/>
+              <input type="checkbox" value="Clothes" onClick={() => updateFilter("Clothes", "product_type")}/> 
+              <label> Clothes </label><br/>
+              <input type="checkbox" value="Accessories" onClick={() => updateFilter("Accessories", "product_type")}/> 
+              <label> Accessories </label><br/>
+              <input type="checkbox" value="Fitness" onClick={() => updateFilter("Fitness", "product_type")}/> 
+              <label> Fitness </label><br/>
+              <input type="checkbox" value="Other" onClick={() => updateFilter("Other", "product_type")}/> 
+              <label> Other </label><br/>
+              
+              <h3>Release Time:</h3>
+              <input type="checkbox" value="8/14" onClick={() => updateFilter("8/14", "release_time")}/> 
+              <label> 8/14 </label><br/>
+              <input type="checkbox" value="8/15" onClick={() => updateFilter("8/15", "release_time")}/> 
+              <label> 8/15 </label><br/>
 
-            <h3>Processor:</h3>
-            <input type = "checkbox" value = "ReStore-Tulsa" onClick = {() => updateFilter("ReStore-Tulsa", "processor")}/> 
-            <label> ReStore-Tulsa </label><br/>
-            <input type = "checkbox" value = "ReStore Rack" onClick = {() => updateFilter("ReStore Rack", "processor")}/> 
-            <label> ReStore Rack </label><br/>
-            <input type = "checkbox" value = "ReStore Broken Arrow" onClick = {() => updateFilter("ReStore Broken Arrow", "processor")}/> 
-            <label> ReStore Broken Arrow </label><br/>
-            <input type = "checkbox" value = "ReStore Claremore" onClick = {() => updateFilter("ReStore Claremore", "processor")}/> 
-            <label> ReStore Claremore </label><br/>
-          </div>
-        
-          <div>
-            <h3>Favorites:</h3>
-            <label>Total Price: ${total}</label>
-          </div>
-          <button onClick = {() => resetPage()}>{"Reset"}</button>
-        </form>
-      </div>
-    </div>);
+              <h3>Processor:</h3>
+              <input type="checkbox" value="ReStore-Tulsa" onClick={() => updateFilter("ReStore-Tulsa", "processor")}/> 
+              <label> ReStore-Tulsa </label><br/>
+              <input type="checkbox" value="ReStore Rack" onClick={() => updateFilter("ReStore Rack", "processor")}/> 
+              <label> ReStore Rack </label><br/>
+              <input type="checkbox" value="ReStore Broken Arrow" onClick={() => updateFilter("ReStore Broken Arrow", "processor")}/> 
+              <label> ReStore Broken Arrow </label><br/>
+              <input type="checkbox" value="ReStore Claremore" onClick={() => updateFilter("ReStore Claremore", "processor")}/> 
+              <label> ReStore Claremore </label><br/>
+            </div>
+          
+            <div>
+              <h3>Favorites:</h3>
+              <label>Total Price: ${total}</label>
+            </div>
+            <button onClick={() => resetPage()}>{"Reset"}</button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
