e87321dcd88c509f99cc365414e24e9bb55da766
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2024-08-21 10:32:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App() {
     </nav>
       <div className="product-cards">
         <h1>Green Country ReStores  
-          <img src="https://images.squarespace-cdn.com/content/v1/60148fcfca55b203f218fe44/49cb0b5c-21d6-4bf8-8f74-544cd98ae908/GC_ReStoreLogoBlackHome.png" 
+          #<img src="https://images.squarespace-cdn.com/content/v1/60148fcfca55b203f218fe44/49cb0b5c-21d6-4bf8-8f74-544cd98ae908/GC_ReStoreLogoBlackHome.png" 
             height="66" padding="100" alt="ReStore Logo"></img>
         </h1>
         <button onClick={() => setShowNav(!showNav)} className="toggle-button">
