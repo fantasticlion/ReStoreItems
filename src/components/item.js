b0a831e85e2284ff4f1fda45cb29ac39ede88b70
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2024-08-22 10:09:28 -0500

    Update item.js

--- src/components/item.js
+++ src/components/item.js
@@ -2,8 +2,8 @@ export default function ProductItem(props) {
   return (
     <div className="item">
       <h2>{props.info["name"]}</h2>
-      <img src={props.info["image_link"]}></img>
       <ul>
+        <li><img src={props.info["image_link"]}></img></li>
         <li>Product Type: {props.info["product_type"]}</li>
         <li>Processor: {props.info["processor"]}</li>
         <li>Release Time: {props.info["release_time"]}</li>
